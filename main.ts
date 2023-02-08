af0416832fd4aed0230927f042fee4f3efac3acb
Pijarro <124789714+Pijarro@users.noreply.github.com>
2023-02-08 13:58:35 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    número_introducido += 10
+})
+input.onGesture(Gesture.Shake, function () {
+    número_introducido = 0
+})
+input.onButtonPressed(Button.B, function () {
+    número_introducido += 1
+})
 let número_introducido = 0
+número_introducido = 0
 basic.showString("" + (número_introducido))
 basic.forever(function () {
-	
+    basic.showString("" + (número_introducido))
 })
